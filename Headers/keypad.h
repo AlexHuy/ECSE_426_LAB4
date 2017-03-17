4054c225ce7df28b004d4c06aa8f4e4fffd22942
MichaelMCP <mcpmichaelmcp@gmail.com>
2017-03-17 11:17:06 -0400

    Add files via upload

---
+++ Headers/keypad.h
@@ -0,0 +1,24 @@
+#ifndef KEYPAD_H
+#define KEYPAD_H
+
+#include <stdint.h>
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_gpio.h"
+
+void init_rows(void);
+
+void init_columns(void);
+
+void delay(uint32_t time);
+
+uint8_t read_rows(void);
+
+uint8_t read_columns(void);
+
+int8_t determine_key(uint8_t row, uint8_t column);
+
+int return_key(void);
+
+void order_key(int* values);
+
+#endif
